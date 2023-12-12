dd83e2a8b9fe887bcc813c3e55230ea901099257
Your name01~ <Your email>
2023-12-12 15:30:24 +0100

    correction majuscule PatientScreen

--- App.js
+++ App.js
@@ -19,8 +19,7 @@ import users from './reducers/users';
 import ManagementScreen from './screens/ManagementScreen';
 import JoinScreen from './screens/JoinScreen';
 import RessourcesScreen from './screens/RessourcesScreen';
-import PatientScreen from './screens/PatientScreen';
-import ConsultationScreen from './screens/ConsultationScreen';
+
 
 
 const reducers = combineReducers({users});
