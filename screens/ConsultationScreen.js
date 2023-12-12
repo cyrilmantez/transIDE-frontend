9205e02c8707cfff670a2fe27ec4769d7b215fbd
Your name01~ <Your email>
2023-12-12 14:48:25 +0100

    patientscreen cyril

---
+++ screens/ConsultationScreen.js
@@ -0,0 +1,10 @@
+import { Button, StyleSheet, Text, View, TouchableWithoutFeedback, KeyboardAvoidingView, ScrollView, Image, Keyboard, TouchableOpacity, SafeAreaView } from 'react-native';
+import { TextInput} from 'react-native-paper';
+import { useFonts, Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+import React, { useState } from 'react';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { useDispatch } from 'react-redux';
+
+export default function ConsultationScreen() {
+
+}
