b5ea59ba6393de9c95370e33352d18488a9a673e
Your name01~ <Your email>
2023-12-12 11:19:18 +0100

    mardi 12 decembre matin

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -2,9 +2,34 @@ import { Button, StyleSheet, Text, View, TouchableWithoutFeedback, KeyboardAvoid
 import { useFonts, Poppins_400Regular } from '@expo-google-fonts/poppins';
 import React, { useState } from 'react';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { useDispatch } from 'react-redux';
 
 export default function SignInScreen() {
   const [passwordVisible, setPasswordVisible] = useState(true);
+  const dispatch = useDispatch();
+  const [signInName, setSignInName] = useState('');
+  const [signInPassword, setSignInPassword] = useState('');
+
+  const handleConnexion = () => {
+    if (!signInName || !signInPassword) {
+      alert('Champs manquants ou incomplets');
+      return;
+    }
+
+    fetch('/http://192.168.1.5:3000/users/signin', {
+      method: 'POST',
+      headers: {'Content-Type' : 'application/json'},
+      body: JSON.stringify({username: signInName, password: signInPassword})
+    }).then(response => response.json())
+      .then(data => {
+        if (data.result){
+          dispatch(login({username: signInName, token: data.token }));
+          setSignInName('');
+          setSignInPassword('');
+          navigation.navigate('TabNavigator');
+        }
+      })
+  }
 
   let [fontsLoaded] = useFonts({
     Poppins_400Regular,
@@ -32,23 +57,22 @@ export default function SignInScreen() {
                 source={require('../assets/logo.png')}
                     />
               <View style={styles.inputContainer}>  
-                <TextInput placeholder='Ton nom'/>
-                <TextInput placeholder='Ton mot de passe' secureTextEntry={passwordVisible} style={{flex: 1}}
-                />
+                <TextInput placeholder='Ton nom' onChangeText={text => setSignInName(text)} value={signInName}/>
+                <TextInput placeholder='Ton mot de passe' secureTextEntry={passwordVisible} style={{flex: 1}} onChangeText={text => setSignInPassword(text)} value={signInPassword}/>
                 <TouchableOpacity onPress={() => setPasswordVisible(!passwordVisible)}>
                   <FontAwesome name={passwordVisible ? 'eye-slash' : 'eye'} size={24} color='grey' />
                 </TouchableOpacity>
-                <TouchableOpacity>
-                  <Text>Valider</Text>
+                <TouchableOpacity style={styles.button} onPress={() => handleConnexion()}>
+                  <Text style={styles.text}>Valider</Text>
                 </TouchableOpacity>
                 <TouchableOpacity>
                   <Text>Des trous de mémoire ?</Text>
                 </TouchableOpacity>
               </View>
-              <View style={styles.innerContainer}>
-                <Text>Pas encore de compte ?</Text>
-                <TouchableOpacity>
-                  <Text>Créer un compte</Text>
+              <View style={styles.footer}>
+                <Text style={styles.footertext}>Pas encore de compte ?</Text>
+                <TouchableOpacity onPress={() => navigation.navigate('SignUpScreen')}>
+                  <Text style={[styles.footertext, {marginTop: 20, color: 'blue'}]}>Créer un compte</Text>
                 </TouchableOpacity>
               </View>
             </View>
@@ -94,5 +118,26 @@ inputContainer: {
   flex : 1,
   alignItems: 'center',
   justifyContent: 'center',
+},
+button : {
+  backgroundColor: '#99BD8F',
+  width: 350,
+  height: 50,
+  borderRadius: 10,
+  marginTop: 25,
+  justifyContent: 'center',
+  alignItems: 'center',
+},
+text: {
+  fontSize: 17,
+  fontFamily: 'Poppins_600SemiBold', 
+},
+footer: {
+  alignItems: 'center',
+   justifyContent: 'center',
+  marginTop: 50,
+},
+footertext: {
+  fontFamily: 'Poppins_600SemiBold', 
 }
 });
