f297dbf22eb2ce8b6b3102f544457453a1226ed0
Your name <Your email>
2023-12-13 16:32:38 +0100

    modif patient et tourscreen

--- screens/TourScreen.js
+++ screens/TourScreen.js
@@ -5,6 +5,7 @@ import DateTimePicker from '@react-native-community/datetimepicker';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import { useFonts, Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
 import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
+import { useSelector } from 'react-redux';
 
 export default function TourScreen({navigation}) {
   
@@ -12,6 +13,21 @@ export default function TourScreen({navigation}) {
   const [date, setDate] = useState(new Date());
   const [visible, setVisible] = useState(false);
   const [mode, setMode] = useState('');
+  const [allPatients, setAllPatients] = useState()
+
+  const user = useSelector((state) => state.users.value)
+  useEffect(() => {
+
+    fetch('http://192.168.1.5:3000/patients/allPatients', {
+      method: 'POST',
+      headers: {'Content-Type' : 'application/json'},
+      body: JSON.stringify({officeToken: user.officeToken, dateOfToday : new Date(date)})
+    }).then(response => response.json())
+      .then(data => {
+        setAllPatients(data.patientsToSee)
+      })
+  }, [date]);
+
 
   const showPicker = (currentMode) => {
     setVisible(true);
@@ -25,6 +41,7 @@ export default function TourScreen({navigation}) {
 
   const dateChange = (event, selectedDate) => {
     const currentDate = selectedDate;
+    console.log('selectedDate', selectedDate)
     setVisible(false);
     setDate(currentDate);
   };
@@ -35,40 +52,8 @@ export default function TourScreen({navigation}) {
     setDate(newDate);
   }
 
-
-  // liste patient du jour
-
-  const allpatients = [
-    { id: '1', nom: 'Patient 1', soin: 'Soin 1', adresse: 'Adresse 1', date: new Date('2023-12-12'), heure: '10:00' },
-    { id: '2', nom: 'Patient 2', soin: 'Soin 2', adresse: 'Adresse 2', date: new Date('2023-12-12'), heure: '11:00' },
-    { id: '3', nom: 'Patient 3', soin: 'Soin 3', adresse: 'Adresse 3', date: new Date('2023-12-13'), heure: '12:00' },
-    { id: '4', nom: 'Patient 4000000000000000000000', soin: 'Soin 4', adresse: 'Adresse 4', date: new Date('2023-12-13'), heure: '13:00' },
-    { id: '5', nom: 'Patient 5', soin: 'Soin 5', adresse: 'Adresse 5', date: new Date('2023-12-13'), heure: '14:00' },
-    { id: '6', nom: 'Patient 6', soin: 'Soin 6', adresse: 'Adresse 6', date: new Date('2023-12-13'), heure: '15:00' },
-    { id: '7', nom: 'Patient 7', soin: 'Soin 7', adresse: 'Adresse 7', date: new Date('2023-12-13'), heure: '16:00' },
-    { id: '8', nom: 'Patient 8', soin: 'Soin 8', adresse: 'Adresse 8', date: new Date('2023-12-13'), heure: '17:00' },
-    { id: '9', nom: 'Patient 9', soin: 'Soin 9', adresse: 'Adresse 9', date: new Date('2023-12-13'), heure: '18:00' },
-    { id: '10', nom: 'Patient 1000000', soin: 'Soin 10', adresse: 'Adresse 10', date: new Date('2023-12-13'), heure: '19:00' },
-  ];
-  
-  // const [patients, setPatients] = useState([]);
-
-  // useEffect(() => {
-  //   fetch('///patient')
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       setPatients(data.patient);
-  //     });
-  // }, []);
-
-  const dateDuJour = new Date(date);
-  dateDuJour.setHours(0, 0, 0, 0);
-
-  const allDayPatients = allpatients.filter(patient => {
-    const datePatient = new Date(patient.date);
-    datePatient.setHours(0, 0, 0, 0);
-    return datePatient.getTime() === dateDuJour.getTime();
-  }).map((patient, i) => {
+  if (allPatients) {
+    const allDayPatients = allPatients.map((patient, i) => {
     let truncatedNom;
     if (patient.nom.length > 15) {
       truncatedNom = `${patient.nom.substring(0, 15)}...`;
@@ -86,10 +71,12 @@ export default function TourScreen({navigation}) {
                 <Paragraph style={styles.nompatient}>{truncatedNom}</Paragraph>
               </View>
               <View>
-                <FontAwesome  name={'map-pin'} size={24} color='#99BD8F' />
+                <TouchableOpacity>
+                  <FontAwesome  name={'map-pin'} size={24} color='#99BD8F' />
+                </TouchableOpacity>
               </View>
               <View>
-                <FontAwesome name={'user'} size={24} color='#99BD8F' />
+                <FontAwesome name={'user'} size={24} color='#99BD8F' onPress={() => navigation.navigate('PatientScreen', { _id: patient._id})}/>
               </View>
               <View>
                 <FontAwesome name={'square-o'} size={24} color='#99BD8F' />
@@ -99,6 +86,8 @@ export default function TourScreen({navigation}) {
         </View>
     );
   });
+  };
+  
 
   let [fontsLoaded] = useFonts({
     Poppins_400Regular,
