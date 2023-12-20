1d7c68fc0831001b2651004b115d81fca011c81c
Your name01~ <Your email>
2023-12-20 20:28:46 +0100

    fin de journée mercredi 20 décembre : MenuScreen terminée et liens vers les pages cibles commencés

--- screens/MenuScreen.js
+++ screens/MenuScreen.js
@@ -1,5 +1,4 @@
 import { Button, StyleSheet, Text, View, TouchableWithoutFeedback, KeyboardAvoidingView, ScrollView, Keyboard, TouchableOpacity, SafeAreaView, StatusBar } from 'react-native';
-import { Button, StyleSheet, Text, View, TouchableWithoutFeedback, KeyboardAvoidingView, ScrollView, Keyboard, TouchableOpacity, SafeAreaView, StatusBar } from 'react-native';
 import { useFonts, Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
 import React, { useState, useEffect } from 'react';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
@@ -10,11 +9,42 @@ export default function MenuScreen({ navigation, route }) {
     const user = useSelector((state) => state.users.value);
     const officeToken = useSelector((state) => state.users.value.officesTokens[0].token);
 
+    // Accès à MyAccountScreen (vérifier si les paramètres envoyés sont suffisants et bien récupérés) :
     const toMyAccount = () => {
-        navigation.navigate('MyAccountScreen', {username : user.username, officeToken: user.officesTokens[0].token, officeName: user.officesTokens[0].name, isByDefault: user.officesTokens[0].isByDefault})
+        navigation.navigate('Mon compte', {
+            username : user.username,
+            officeToken: user.officesTokens[0].token,
+            officeName: user.officesTokens[0].name,
+            isByDefault: user.officesTokens[0].isByDefault
+        })
     };
 
+    const toMyFirm = () => {
+        navigation.navigate('ManagementScreen', {
+            username : user.username,
+            officeToken: user.officesTokens[0].token,
+            officeName: user.officesTokens[0].name,
+            isByDefault: user.officesTokens[0].isByDefault
+        })
+    };
     
+    /* const toJoinATeam = () => {
+        navigation.navigate('XXX', {
+            username : user.username,
+            officeToken: user.officesTokens[0].token,
+            officeName: user.officesTokens[0].name,
+            isByDefault: user.officesTokens[0].isByDefault
+        })
+    }; */
+
+    const toResources = () => {
+        navigation.navigate('RessourcesScreen', {
+            username : user.username,
+            officeToken: user.officesTokens[0].token,
+            officeName: user.officesTokens[0].name,
+            isByDefault: user.officesTokens[0].isByDefault
+        })
+    };
 
     let [fontsLoaded] = useFonts({
         Poppins_400Regular,
@@ -31,13 +61,13 @@ export default function MenuScreen({ navigation, route }) {
                 <StatusBar barStyle="light-content"/>
                     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
                         <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-                            <ScrollView contentContainerStyle={styles.scrollView}>
+                            <ScrollView >
                                 <View style={styles.chevron}> 
                                     <TouchableOpacity onPress={() => {navigation.navigate('TabNavigator')}}>
                                         <FontAwesome name={'chevron-left'} size={30} color='white' />
                                     </TouchableOpacity>
                                 </View>
-                                <View styles={styles.titleContainer}>
+                                <View style={styles.titleContainer}>
                                     <FontAwesome name='bars' size={32} color='white'/>
                                     <Text style={styles.titlePage}>  MENU</Text>
                                 </View>
@@ -48,13 +78,19 @@ export default function MenuScreen({ navigation, route }) {
                                         </TouchableOpacity>
                                     </View>
                                     <View>
-                                        <Text style={styles.choiceText}>Gérer mon cabinet</Text>
+                                        <TouchableOpacity onPress={() => toMyFirm()}>
+                                            <Text style={styles.choiceText}>Gérer mon cabinet</Text>
+                                        </TouchableOpacity>
                                     </View>
                                     <View>
-                                        <Text style={styles.choiceText}>Rejoindre une équipe</Text>
+                                        <TouchableOpacity /* onPress={() => toJoinATeam()} */>
+                                            <Text style={styles.choiceText}>Rejoindre une équipe</Text>
+                                        </TouchableOpacity>
                                     </View>
                                     <View>
-                                        <Text style={styles.choiceText}>Ressources</Text>
+                                        <TouchableOpacity onPress={() => toResources()}>
+                                            <Text style={styles.choiceText}>Ressources</Text>
+                                        </TouchableOpacity>
                                     </View>
                                 </View>
                             </ScrollView>
@@ -69,51 +105,47 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#99BD8F',
-        //alignItems: 'center',
         justifyContent: 'center',
-        alignItems: 'center',
-    },
-    scrollView: {
-        
-               
+        //alignItems: 'center',
     },
     chevron: {
-        alignSelf: 'flex-start',
-        marginLeft: 20,
+        alignContent: 'flex-start',
+        marginTop: 40,
         marginBottom: 40,
+        paddingLeft: 20,
+        /* borderColor: 'red',
+        borderWidth: 4, */
      },
     titleContainer: {
-        //flexDirection: 'row',
-        //justifyContent: 'space-between',
-        width: 300,
-        height: 300,
-        borderColor: 'red',
-        borderWidth: 4,
-        //fontFamily: 'Poppins_600SemiBold',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'baseline',
+        //borderColor: 'red',
+        //borderWidth: 4,
+        //padding: 15,
     },
     titlePage: {
-        //flexDirection: 'row',
-        width: '100%',
         marginBottom: 20,
         color: 'white',
         fontSize: 30,
         fontFamily: 'Poppins_600SemiBold',
-        textAlign: 'center',
     },
-    text: {
-
+   /*  text: {
         color: 'white',
         fontSize: 20,
         fontWeight: '400',
-    },
+    }, */
     menuContainer: {
-        //flex: 2,
-        width: '80%',
-        alignItems: 'flex-start',
+        /* borderColor: 'red',
+        borderWidth: 4, */
+        paddingLeft: 40,
     },
     choiceText: {
+        /* borderColor: 'red',
+        borderWidth: 4, */
         color: 'white',
-        fontSize: 18,
+        fontSize: 20,
         marginTop: 20,
+        marginBottom: 20,
     },
 });
